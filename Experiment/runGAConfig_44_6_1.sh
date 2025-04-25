004a1f826b6c07b231fa2b2625d1e6e6a685f95f
meshun074 <eshunmichael2tech1@yahoo.com>
2025-04-24 21:20:51 -0400

    generate config file and updated the java program to accept a single config file to run

---
+++ Experiment/runGAConfig_44_6_1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~/MultiThreadedHHCRSP/
+java -cp lib/jackson-databind-2.18.2.jar:out/artifacts/GAforHHCRSP_jar/GAforHHCRSP.jar:lib/jackson-annotations-2.18.2.jar:lib/jackson-core-2.18.2.jar org.example.Main Experiment/configs/GAConfig_44_6_1.json
+
