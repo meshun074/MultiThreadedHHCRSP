1797c04f75d39f5e81d27a081cada76f8450df2f
meshun074 <eshunmichael2tech1@yahoo.com>
2025-06-03 17:14:37 -0400

    Takes two commandline argument

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -88,7 +88,7 @@ public class Main {
                 double total = 0;
                 double best = Double.MAX_VALUE;
 
-                GeneticAlgorithm ga = new GeneticAlgorithm(randomSeed, 6, 10, 4, 100, 6000, 0.1f, 1.0f, p, instance);
+                GeneticAlgorithm ga = new GeneticAlgorithm(randomSeed, 6, 10, 4, 300, 600, 0.1f, 1.0f, p, instance);
                 Chromosome bestChromosome = ga.start();
                 // Execute GA tasks
 
